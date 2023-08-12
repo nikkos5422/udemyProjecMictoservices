983291738dc41fa9daf5de8ebe6d37a148b33e38
Nikolai Kosariev <kosarnik@fit.cvut.cz>
2023-08-12 21:33:33 +0200

    added feign

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/AlbumServiceClient.java
@@ -1,14 +1,24 @@
 package com.appsdeveloperblog.photoapp.api.users.data;
 
 import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @FeignClient(name = "albums-ws")
 public interface AlbumServiceClient {
     @GetMapping("/users/{id}/albums")
+    @CircuitBreaker(name="albums-ws", fallbackMethod="getAlbumsFallback")
     List<AlbumResponseModel> getAlbums(@PathVariable String id);
+
+    default List<AlbumResponseModel> getAlbumsFallback(String id,Throwable exception) {
+        System.out.println("\n id="+id);
+        System.out.println("\n exception="+exception.getMessage());
+        return new ArrayList<>();
+
+    }
 }
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/security/WebSecurity.java
@@ -1,9 +1,10 @@
 package com.appsdeveloperblog.photoapp.api.users.security;
 
 import com.appsdeveloperblog.photoapp.api.users.service.UsersService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private final UsersService usersService;
@@ -20,16 +20,25 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final Environment environment;
 
+    @Autowired
+    public WebSecurity(UsersService usersService, BCryptPasswordEncoder bCryptPasswordEncoder, Environment environment) {
+        this.usersService = usersService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.environment = environment;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip")).and()
+        http.authorizeRequests()
+                .antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
+                .antMatchers(HttpMethod.GET,"/actuator/health").hasIpAddress(environment.getProperty("gateway.ip"))
+                .antMatchers(HttpMethod.GET,"/actuator/circuitbreakerevents ").hasIpAddress(environment.getProperty("gateway.ip")).and()
                 .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
     }
 
-    private AuthenticationFilter getAuthenticationFilter() throws Exception
-    {
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager());
         //authenticationFilter.setAuthenticationManager(authenticationManager());
         authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
